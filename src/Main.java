c04838db5a5183062b1b8654fcd18a8ec37ca07a
Heshan-w <htwanigasinghe@gmail.com>
2023-12-21 17:01:36 +0530

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("r u manager or customer");
+        Scanner scanner = new Scanner(System.in);
         WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
         westminsterShoppingManager.displayManagerConsole();
         System.out.println("Thank you for using Westminster Shopping Manager!");
