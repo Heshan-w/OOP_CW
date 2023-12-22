6bfd07f7081ea82abe8a45f0444c48b83056aef1
Heshan-w <htwanigasinghe@gmail.com>
2023-12-23 02:24:37 +0530

    Main class has functionality to return to Main class menu after manager accessing manager console

--- src/Main.java
+++ src/Main.java
@@ -9,39 +9,57 @@ public class Main {
         String choice;
 
         System.out.print("""
-                To access the customer console, please enter 1.
-                To access the manager console, please enter 2.
-                Enter your choice (1 or 2):\s""");
-
+                    To access the customer console, please enter 1.
+                    To access the manager console, please enter 2.
+                    Enter your choice (1 or 2):\s""");
         choice = scanner.next();
-        if (choice.equals("1")) {
-            System.out.println("You have selected the customer console.");
-            System.out.println("Please enter your username and password to login.");
-            System.out.print("Username: ");
-            String username = scanner.next();
-            System.out.print("Password: ");
-            String password = scanner.next();
-            User customer = new User(username, password);
 
-            // Create an instance of WestminsterShoppingManager
-            WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
-            // Add products to the availableProducts list
-            westminsterShoppingManager.addProduct();
-            westminsterShoppingManager.addProduct();
-            // Get the availableProducts list
-            List<Product> availableProducts = westminsterShoppingManager.getStoreInventory();
+        do {
+            switch (choice) {
+                case "1" -> {
+                    System.out.println("You have selected the customer console.");
+                    System.out.println("Please enter your username and password to login.");
+                    System.out.print("Username: ");
+                    String username = scanner.next();
+                    System.out.print("Password: ");
+                    String password = scanner.next();
+                    User customer = new User(username, password);
 
-            // Launch the customer GUI with the availableProducts list
-            launchCustomerGUI(customer, availableProducts);
-        } else if (choice.equals("2")) {
-            System.out.println("You have selected the manager console.");
-            WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
-            westminsterShoppingManager.displayManagerConsole();
-        } else {
-            System.out.println("Invalid choice. Please enter a valid option (1 or 2).");
-        }
+                    // Create an instance of WestminsterShoppingManager
+                    WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
+                    // Add products to the availableProducts list
+                    westminsterShoppingManager.addProduct();
+                    westminsterShoppingManager.addProduct();
+                    // Get the availableProducts list
+                    List<Product> availableProducts = westminsterShoppingManager.getStoreInventory();
 
-        System.out.println("Thank you for using Westminster Shopping Manager!");
+                    // Launch the customer GUI with the availableProducts list
+                    launchCustomerGUI(customer, availableProducts);
+                }
+                case "2" -> {
+                    System.out.println("You have selected the manager console.");
+                    WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
+                    westminsterShoppingManager.displayManagerConsole();
+                }
+                default -> {
+                    System.out.println("Invalid choice. Please enter a valid option (1 or 2).");
+                    System.out.print("Enter your choice here (1 or 2): ");
+                    choice = scanner.next();
+                }
+            }
+            System.out.print("""
+                    \nTo access the customer console, please enter 1.
+                    To access the manager console, please enter 2.
+                    To exit the application, please enter 3.
+                    Enter your choice (1, 2 or 3):\s""");
+            choice = scanner.next();
+
+            if (choice.equals("3")) {
+                System.out.println("""
+                        \nThank you for using Westminster Shopping Manager!
+                        Exiting the application....""");
+            }
+        } while (!choice.equals("3"));
     }
 
     private static void launchCustomerGUI(User customer, List<Product> availableProducts) {
